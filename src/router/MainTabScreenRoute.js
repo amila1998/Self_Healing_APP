614525c1354a8a7a08ceb7017bbeb343aabf2000
sean-udayantha <seanudayantha@mail.com>
2022-11-11 14:17:07 +0530

    investor gig frontend

--- src/router/MainTabScreenRoute.js
+++ src/router/MainTabScreenRoute.js
@@ -17,6 +17,9 @@ import MyBusinesses from '../screen/businesses/MyBusinesses';
 import AddNewBusiness from '../screen/businesses/AddNewBusiness';
 import MyBusinessDetails from '../screen/businesses/MyBusinessDetails';
 import BusinessDetails from '../screen/businesses/BusinessDetails';
+import ViewInvestorGigs from '../screen/investorGigs/ViewInvestorGigs';
+import Addgig from '../screen/investorGigs/Addgig';
+import EditGig from '../screen/investorGigs/EditGig';
 
 // import HomeScreen from './HomeScreen';
 // import DetailsScreen from './DetailsScreen';
@@ -114,6 +117,18 @@ const HomeStackScreen = ({navigation}) => (
         }} />
           <HomeStack.Screen name="BusinessDetails" component={BusinessDetails}  options={{
         title:'Business Details',
+        headerShown: true
+        }} />
+        <HomeStack.Screen name="ViewInvestorGigs" component={ViewInvestorGigs}  options={{
+        title:"",
+        headerShown: true
+        }} />
+        <HomeStack.Screen name="Addgig" component={Addgig}  options={{
+        title:"",
+        headerShown: true
+        }} />
+        <HomeStack.Screen name="EditGig" component={EditGig}  options={{
+        title:"",
         headerShown: true
         }} />
     </HomeStack.Navigator>
