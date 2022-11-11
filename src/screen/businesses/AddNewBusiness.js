b3eff60bdd8bd0be5cec2806d55adec43e3bfd62
amila1998 <amiladevin@gmail.com>
2022-11-11 06:40:04 +0530

    My Business

--- src/screen/businesses/AddNewBusiness.js
+++ src/screen/businesses/AddNewBusiness.js
@@ -1,6 +1,6 @@
 import { LinearGradient } from 'expo-linear-gradient'
 import React, { useState } from 'react'
-import { Image, SafeAreaView, ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
+import { Alert, Image, SafeAreaView, ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
 import bGStyles from '../../Styles/Background'
 import Colors from '../../Styles/Colors'
 import SelectList from 'react-native-dropdown-select-list'
@@ -8,11 +8,20 @@ import BusinessStatusData from '../../Data/BusinessStatus'
 import BusinessCategoriesData from '../../Data/BusinessCategories'
 import BusinessExpectationsData from '../../Data/BusinessExpectations'
 import MultiSelect from 'react-native-multiple-select';
+import { useLogin } from '../../context/LoginProvider'
+import client from '../../apiRouter/client'
+import { useNavigation } from '@react-navigation/native'
 
 const AddNewBusiness = () => {
+ 
   const [selectedItems, setSelectedItems] = useState([]);
-  console.log("🚀 ~ file: addNewBusiness.js ~ line 14 ~ AddNewBusiness ~ selectedItems", selectedItems)
-
+  const [business_name, setbusiness_name] = useState('')
+  const [location, setlocation] = useState('')
+  const [status, setstatus] = useState('')
+  const [category, setcategory] = useState('')
+  const [description, setdescription] = useState('')
+  const { token,setCallBack } = useLogin()
+  const Navigator = useNavigation();
 
   const onSelectedItemsChange = (selectedItems) => {
 
@@ -20,10 +29,29 @@ const AddNewBusiness = () => {
 
     for (let i = 0; i < selectedItems.length; i++) {
       var tempItem = BusinessExpectationsData.find(item => item.id === selectedItems[i]);
-      console.log(tempItem);
+      //console.log(tempItem);
     }
 
   };
+
+  const addNewBusiness = async (e) => {
+    e.preventDefault();
+    try {
+      setCallBack(true)
+      const res = await client.post('/api/business/add', { business_name, location, status, category, expectations: selectedItems,description }, {
+        headers: { Authorization: token }
+      })
+      console.log("🚀 ~ file: addNewBusiness.js ~ line 42 ~ addNewBusiness ~ res", res)
+      
+      Navigator.navigate('MyBusinesses')
+
+    } catch (error) {
+      console.log("🚀 ~ file: addNewBusiness.js ~ line 38 ~ addNewBusiness ~ error", error.response.data)
+      Alert.alert(
+        error.response.data.message? error.response.data.message: error.response.data.msg
+      )
+    }
+  }
   return (
     <LinearGradient
       // Background Linear Gradient
@@ -44,73 +72,65 @@ const AddNewBusiness = () => {
             </View>
             <View style={styles.loginBody}>
               <View style={styles.formInput}>
-                <TextInput style={styles.textInput} placeholder='Business Name'></TextInput>
+                <TextInput onChangeText={setbusiness_name} style={styles.textInput} placeholder='Business Name'></TextInput>
               </View>
               <View style={styles.formInput}>
-                <TextInput style={styles.textInput} placeholder='Location'></TextInput>
+                <TextInput onChangeText={setlocation} style={styles.textInput} placeholder='Location'></TextInput>
               </View>
               <View style={styles.formInput}>
-                <SelectList search={false} dropdownStyles={{ backgroundColor: '#fff' }} boxStyles={styles.textInput} data={BusinessStatusData} placeholder='Business Status' />
+                <SelectList setSelected={setstatus} search={false} dropdownStyles={{ backgroundColor: '#fff' }} boxStyles={styles.textInput} data={BusinessStatusData} placeholder='Business Status' />
               </View>
               <View style={styles.formInput}>
-                <SelectList search={true} dropdownStyles={{ backgroundColor: '#fff' }} boxStyles={styles.textInput} data={BusinessCategoriesData} placeholder='Business Category' />
+                <SelectList setSelected={setcategory} search={true} dropdownStyles={{ backgroundColor: '#fff' }} boxStyles={styles.textInput} data={BusinessCategoriesData} placeholder='Business Category' />
               </View>
-                <ScrollView horizontal={true} style={{ width: "100%", marginBottom: 10, padding: 10, }}>
-                  <MultiSelect
+              <ScrollView horizontal={true} style={{ width: "100%", marginBottom: 10, padding: 10, }}>
+                <MultiSelect
                   hideTags
-                    items={BusinessExpectationsData}
-                    uniqueKey="id"
-                    onSelectedItemsChange={onSelectedItemsChange}
-                    selectedItems={selectedItems}
-                    selectText="Business Expectations"
-                    searchInputPlaceholderText="Search business expectations Here..."
-                    tagRemoveIconColor="red"
-                    tagTextColor="#fff"
-                    selectedItemTextColor="#CCC"
-                    selectedItemIconColor="#CCC"
-                    itemTextColor="#000"
-                    displayKey="name"
-                    searchInputStyle={{ color: '#000' ,  marginLeft: 10,padding:10}}
-                    submitButtonColor="#FFB801"
-                    submitButtonText="Submit"
-                    tagContainerStyle={styles.tag}
-                      styleDropdownMenu={{
-                      borderWidth: 1,
-                      borderColor: '#10006b',
-                      borderRadius: 50,
-                      }}
-                      styleDropdownMenuSubsection={{
-                      borderWidth: 0.5,
-                      borderColor: '#10006b',
-                      borderRadius: 50,
-                      padding:10,
-                      width: '100%',
-                      maxWidth:310
-                      
-                      }}
-                      styleMainWrapper={{
-                        width: '100%',
-                      }}
-                      styleTextDropdownSelected={{
-                        maxWidth:310,
-                        width: '100%',
-                      }}
-                
-                      
-                      
-                    
-                 
-                   
-                  />
+                  items={BusinessExpectationsData}
+                  uniqueKey="id"
+                  onSelectedItemsChange={onSelectedItemsChange}
+                  selectedItems={selectedItems}
+                  selectText="Business Expectations"
+                  searchInputPlaceholderText="Search business expectations Here..."
+                  tagRemoveIconColor="red"
+                  tagTextColor="#fff"
+                  selectedItemTextColor="#CCC"
+                  selectedItemIconColor="#CCC"
+                  itemTextColor="#000"
+                  displayKey="name"
+                  searchInputStyle={{ color: '#000', marginLeft: 10, padding: 10 }}
+                  submitButtonColor="#FFB801"
+                  submitButtonText="Submit"
+                  tagContainerStyle={styles.tag}
+                  styleDropdownMenu={{
+                    borderWidth: 1,
+                    borderColor: '#10006b',
+                    borderRadius: 50,
+                  }}
+                  styleDropdownMenuSubsection={{
+                    borderWidth: 0.5,
+                    borderColor: '#10006b',
+                    borderRadius: 50,
+                    padding: 10,
+                    width: '100%',
+                    maxWidth: 310
 
-
-                </ScrollView>
+                  }}
+                  styleMainWrapper={{
+                    width: '100%',
+                  }}
+                  styleTextDropdownSelected={{
+                    maxWidth: 310,
+                    width: '100%',
+                  }}
+                />
+              </ScrollView>
               <View style={styles.formInput}>
-                <TextInput style={styles.textInput} placeholder='Business Description' numberOfLines={4} multiline={true}></TextInput>
+                <TextInput onChangeText={setdescription} style={styles.textInput} placeholder='Business Description' numberOfLines={4} multiline={true}></TextInput>
               </View>
               <View style={styles.formInput}>
-                <TouchableOpacity style={styles.defaultButton}>
-                  <Text style={{ textAlign: 'center', fontSize: 16, color: '#fff' }} >Register</Text>
+                <TouchableOpacity onPress={addNewBusiness} style={styles.defaultButton}>
+                  <Text style={{ textAlign: 'center', fontSize: 16, color: '#fff' }} >Submit</Text>
                 </TouchableOpacity>
               </View>
             </View>
@@ -122,8 +142,8 @@ const AddNewBusiness = () => {
   )
 }
 const styles = StyleSheet.create({
-  tag:{
-    backgroundColor:'#2A2B5F',
+  tag: {
+    backgroundColor: '#2A2B5F',
   },
   mainContainer: {
     marginTop: 18,
--- src/screen/businesses/MyBusinesses.js
+++ src/screen/businesses/MyBusinesses.js
@@ -1,13 +1,52 @@
 import { LinearGradient } from 'expo-linear-gradient'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Image, SafeAreaView, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import { useLogin } from '../../context/LoginProvider'
 import bGStyles from '../../Styles/Background'
 import Colors from '../../Styles/Colors'
 import { useNavigation } from '@react-navigation/native'
-const MyBusinesses = () => {
-    const { token } = useLogin();
+import client from '../../apiRouter/client'
+const MyBusinesses = ({ route }) => {
+    console.log("🚀 ~ file: MyBusinesses.js ~ line 9 ~ MyBusinesses ~ route", route)
+    //const { callback } = route.params;
+    // const [callback, setCallBack] = useState(true)
+    // const [callback2, setCallBack2] = useState(false)
+    //console.log("🚀 ~ file: MyBusinesses.js ~ line 10 ~ MyBusinesses ~ callback", callback)
+    const { token, profile, callback, setCallBack } = useLogin();
     const Navigator = useNavigation();
+    const [businesses, getBusinesses] = useState([])
+    const [mybusinesses, setMyBusinesses] = useState([])
+    console.log("🚀 ~ file: MyBusinesses.js ~ line 18 ~ MyBusinesses ~ mybusinesses", mybusinesses)
+
+
+
+
+    useEffect(() => {
+        const getAllBusiness = async () => {
+            try {
+                const res = await client.get('/api/business/viewAll')
+                getBusinesses(res.data)
+                let myBusiness = []
+                for (const b of res.data) {
+                    if (b.userID === profile._id) {
+                        myBusiness.push(b)
+                    }
+
+                }
+                setMyBusinesses(myBusiness)
+                setCallBack(false)
+
+            } catch (error) {
+                console.log("🚀 ~ file: MyBusinesses.js ~ line 23 ~ getAllBusiness ~ error", error)
+
+            }
+        }
+        if (callback) {
+            getAllBusiness();
+
+        }
+    }, [callback, businesses, profile])
+
     return (
         <LinearGradient
             // Background Linear Gradient
@@ -16,48 +55,52 @@ const MyBusinesses = () => {
         >
             <SafeAreaView style={{ marginTop: 20, flex: 1, }}>
                 <View style={{ flex: 1, }}>
-                    <View style={{ flexDirection: 'row', padding: 5, justifyContent: 'flex-end' , marginRight:10 }}>
+                    <View style={{ flexDirection: 'row', padding: 5, justifyContent: 'flex-end', marginRight: 10 }}>
                         <View style={{ flexDirection: 'row', marginLeft: 10 }}>
-                            <TouchableOpacity onPress={()=>Navigator.navigate('AddNewBusiness')} style={{ flexDirection: 'row' , backgroundColor:'#FF6D27', padding:10, borderRadius:50}} >
+                            <TouchableOpacity onPress={() => Navigator.navigate('AddNewBusiness')} style={{ flexDirection: 'row', backgroundColor: '#FF6D27', padding: 10, borderRadius: 50 }} >
                                 <Text style={{ color: '#fff', fontSize: 16 }}>Add New Business</Text>
                             </TouchableOpacity></View>
                     </View>
                     <View style={{ marginTop: 20, padding: 10 }}>
-                        <ScrollView>
-                            <SafeAreaView style={{ marginBottom: 30 }}>
-                                <View style={[styles.card, styles.elevation]}>
-                                    <View style={styles.ridesFriends}>
-                                        <View style={styles.left}>
-                                            <View style={{ flexDirection: 'row' }}>
-                                                <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                                                    <Image source={require('../../../assets/homeIcons/office-building-icon.png')} style={{ marginRight: 5, width: 40, height: 40 }} />
-                                                    <Text style={styles.cardTitle}>Hashi Cakes</Text>
-                                                </View>
-                                            </View>
-                                            <View style={{ flexDirection: 'row', marginLeft: 40 }}>
-                                                <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                                                    <Text style={{ fontWeight: 'bold', marginRight: 5 }}>Status</Text>
-                                                    <Text style={{ fontWeight: 'bold', marginRight: 5 }}>:</Text>
-                                                    <Text style={{ marginRight: 5 }}>Hope to start</Text>
-                                                </View>
-                                            </View>
-                                            <View style={{ flexDirection: 'row', marginLeft: 40 }}>
-                                                <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                                                    <Text style={{ fontWeight: 'bold', marginRight: 5 }}>Category</Text>
-                                                    <Text style={{ fontWeight: 'bold', marginRight: 5 }}>:</Text>
-                                                    <Text style={{ marginRight: 5 }}>Foods</Text>
-                                                </View>
-                                            </View>
-                                            <View style={{ flexDirection: 'row', marginLeft: 40 }}>
-                                                <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                                                    <Text style={{ fontWeight: 'bold', marginRight: 5 }}>Location</Text>
-                                                    <Text style={{ fontWeight: 'bold', marginRight: 5 }}>:</Text>
-                                                    <Text style={{ marginRight: 5 }}>Panadura</Text>
+                        <ScrollView style={{ marginBottom: 40 }}>
+                            <SafeAreaView >
+                                {
+                                    mybusinesses.map(m => {
+                                        return <View key={m._id} style={[styles.card, styles.elevation]}>
+                                            <View style={styles.ridesFriends}>
+                                                <View style={styles.left}>
+                                                    <View style={{ flexDirection: 'row' }}>
+                                                        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                                                            <Image source={require('../../../assets/homeIcons/office-building-icon.png')} style={{ marginRight: 5, width: 40, height: 40 }} />
+                                                            <Text style={styles.cardTitle}>{m.business_name}</Text>
+                                                        </View>
+                                                    </View>
+                                                    <View style={{ flexDirection: 'row', marginLeft: 10 }}>
+                                                        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                                                            <Text style={{ fontWeight: 'bold', marginRight: 5 }}>Status</Text>
+                                                            <Text style={{ fontWeight: 'bold', marginRight: 5 }}>:</Text>
+                                                            <Text style={{ marginRight: 5 }}>{m.status === 'hope_to_start' && 'Hope to start'}{m.status === 'started_as_new_business' && 'Started as a new business'}{m.status === 'Already_started' && 'Already Started (one month ago)'}</Text>
+                                                        </View>
+                                                    </View>
+                                                    <View style={{ flexDirection: 'row', marginLeft: 10 }}>
+                                                        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                                                            <Text style={{ fontWeight: 'bold', marginRight: 5 }}>Category</Text>
+                                                            <Text style={{ fontWeight: 'bold', marginRight: 5 }}>:</Text>
+                                                            <Text style={{ marginRight: 5, flexWrap: 'wrap' }}>{m.category}</Text>
+                                                        </View>
+                                                    </View>
+                                                    <View style={{ flexDirection: 'row', marginLeft: 10 }}>
+                                                        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                                                            <Text style={{ fontWeight: 'bold', marginRight: 5 }}>Location</Text>
+                                                            <Text style={{ fontWeight: 'bold', marginRight: 5 }}>:</Text>
+                                                            <Text style={{ marginRight: 5 }}>{m.location}</Text>
+                                                        </View>
+                                                    </View>
                                                 </View>
                                             </View>
                                         </View>
-                                    </View>
-                                </View>
+                                    })
+                                }
                             </SafeAreaView>
                         </ScrollView>
 
@@ -133,7 +176,7 @@ const styles = StyleSheet.create({
         backgroundColor: 'white',
         borderRadius: 8,
         paddingVertical: 20,
-        paddingHorizontal: 25,
+        paddingHorizontal: 5,
         width: '100%',
         marginVertical: 10,
         margin: 10,
